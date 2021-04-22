dcb3fd5f571e7b98d56831a39dae3f48a49f4591
Shivan Khullar <khullar@kingcrab.(none)>
2021-04-21 20:37:07 -0400

    Added functionality to take care of duplicate particleIDs. If there are duplicates, the masses are just added together and then they're treated as a single particle.

--- matcher_functions.cpp
+++ matcher_functions.cpp
@@ -221,13 +221,25 @@ double sum_array(std::vector<double> a)
 int compare_particles(std::vector<double> a, std::vector<double> b)
 {
         int count = 0;
+	int dups = 0;
+	std::vector<double> c;
         for (int i=0; i<a.size();i++)
         {
                 for (int j=0; j<b.size(); j++)
                 {
-                        if (a[i] == b[j]) { count = count+1;}
+                        if (a[i] == b[j]) 
+			{ 
+				count = count+1;
+				//for (int k=0; k<c.size(); k++)
+				//{
+				//	if (b[j] == c[k]) {std::cout << "This is a duplicate particle, wtf..." << j << ", " << k << std::endl;
+				//		dups = dups +1;}
+				//}
+				//c.push_back(b[j]);
+			}
                 }
         }
+	//std::cout << "Sizes: " << a.size() << ", " << b.size() << ", dups: " << dups << std::endl; 
 	return count;
 }
 
@@ -252,6 +264,17 @@ void mass_frac(std::vector<double> parent_pIDs, std::vector<double> child_pIDs, 
         }
         childs_mass_frac_from_parent = sum_array(store_child_masses) / sum_array(child_masses);
         parents_mass_frac_to_child = sum_array(store_parent_masses) / sum_array(parent_masses);
+	if (childs_mass_frac_from_parent >=1 || parents_mass_frac_to_child>=1)
+	{
+		std::cout << "Store parent masses:" << store_parent_masses.size() << " " << num_common_particles << " " << parent_masses.size() << std::endl;
+		//print_array_double(store_parent_masses);
+		std::cout << "Store child masses:" << store_child_masses.size() << " " << num_common_particles << " " << child_masses.size() << std::endl;
+		std::cout << "Sums: " << sum_array(store_parent_masses) << " " << sum_array(parent_masses) << std::endl;
+		//print_array_double(store_child_masses);
+
+		//std::cout << "Store parent masses:" << std::endl;
+		//std::cout << " " << std::endl;
+	}
 	//std::cout << "Childs mass frac from parent value: " << childs_mass_frac_from_parent << std::endl;
 	//std::cout << "Parents mass frac to child value: " << parents_mass_frac_to_child <<std::endl;
 }
@@ -261,30 +284,30 @@ std::string get_cloud_name(int i, Params &params)
 {
         std::string cloud_name;
         // customize this part below based on how many clouds there are.
-        if (i<10) { cloud_name = params.cloud_prefix + "00"+ std::to_string(i);}
-        else if (i>=10 & i<100) { cloud_name = params.cloud_prefix + "0"+ std::to_string(i);}
-        else if (i>=100 & i<1000) { cloud_name = params.cloud_prefix + std::to_string(i);}
-        else { cloud_name = params.cloud_prefix + std::to_string(i); std::cout << "Make sure to modify the get_cloud_name() function correctly." << std::endl;}
+        if (i<10) { cloud_name = params.cloud_prefix + "000"+ std::to_string(i);}
+        else if (i>=10 & i<100) { cloud_name = params.cloud_prefix + "00"+ std::to_string(i);}
+        else if (i>=100 & i<1000) { cloud_name = params.cloud_prefix + "0" + std::to_string(i);}
+        else { cloud_name = params.cloud_prefix + std::to_string(i);}
+	// std::cout << "Make sure to modify the get_cloud_name() function correctly." << std::endl;
         return cloud_name;
 }
 
 
-void load_clouds_to_group(int snap_num, int num_clouds_parent_snap, Params &params, CitySnaps &snapsnap, std::string key)
+void load_clouds_to_group(int snap_num, int num_clouds_snap, Params &params, CitySnaps &snapsnap, std::string key)
 {
-        for (int i=0; i<num_clouds_parent_snap; i++)
+        for (int i=0; i<num_clouds_snap; i++)
         {
                 std::string cloud_name = get_cloud_name(i, params);
-                // initiate a cloud instance here through read_cloud_data and then pass data from that like pIDs or masses around
-                //read_cloud_property(snap_num, params, cloud_name, params.key_particle_IDs);
-                //read_cloud_property(snap_num, params, cloud_name, params.key_masses);
 		std::vector<double> masses = read_cloud_data_double(params, snap_num, params.file_arch_masses_field, cloud_name);
 		//print_array_double(masses);
 		std::vector<double> pIDs = read_cloud_data_double(params, snap_num, params.file_arch_pIDs_field, cloud_name);
 		//print_array_double(pIDs);
-                MemberCloud snapcloud(cloud_name, pIDs, masses);
-                if (key.compare("parent")==0) { snapsnap.parent_group.add_member(snapcloud);} 
-			//std::cout << "Added cloud" << i << "to parent group" << std::endl;}
-                else if (key.compare("child")==0) { snapsnap.child_group.add_member(snapcloud);}
+                
+		MemberCloud snapcloud(cloud_name, pIDs, masses);
+                if (key.compare("parent")==0) { snapsnap.parent_group.add_member(snapcloud);   //} 
+			std::cout << "Added Cloud" << i << " to parent group" << std::endl;}
+                else if (key.compare("child")==0) { snapsnap.child_group.add_member(snapcloud);
+			std::cout << "Added Cloud" << i << " to child group" << std::endl;}
                 else { std::cout << "Don't change the format of the key being passed around." << std::endl;}
         }
 }

c8572c77bdfad599e2fcedecf9e5cd97bee5bdc6
Shivan Khullar <shivankhullar@gmail.com>
2023-05-31 18:49:44 -0400

    some cleanup

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -9,24 +9,28 @@
 #include <cstring>
 #include <fstream>
 
+using namespace std;
 
 int main(int argc, char* argv[])
 {
 	Params params;
-        params.first_snap = 590; params.last_snap = 599;
+        params.first_snap = 601; params.last_snap = 614;
+        string name;
+	name = "n10_alpha2";
+	params.path = "../sample_data/m12i_no_fb_3/"+name+"/";
         params.cloud_prefix = "Cloud";
         params.dat_filename_base_prefix = "bound_";
-        params.dat_filename_base_suffix = "_n10_alpha2.dat";
+        params.dat_filename_base_suffix = "_"+name+".dat";
         params.filename_base_prefix = "Clouds_";
-        params.filename_base_suffix = "_n10_alpha2.hdf5";
-        params.path = "../sample_data/";
+        params.filename_base_suffix = "_"+name+".hdf5";
         params.file_arch_root = "/";
         params.file_arch_cloud_subgroup = "PartType0";
         params.file_arch_masses_field = "Masses";
         params.file_arch_pIDs_field = "ParticleIDs";
         params.write_filename_base_prefix = "Tracked_Clouds_";
-        params.write_filename_base_suffix = "_n10_alpha2.hdf5";
+        params.write_filename_base_suffix = "_"+name+".hdf5";
         params.particle_lower_limit = 32;
-	params.linker_output_filename_prefix = "Linked_Clouds_";
+	params.threshold_frac_for_child=0.75;
+	params.linker_output_filename_prefix = "Linked_Clouds_"+name+"_thresh0.75_";
 	linker(params);
 }

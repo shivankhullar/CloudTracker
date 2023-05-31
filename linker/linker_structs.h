25001ac64c650e0b39cb2e418bbf8d323c3e5f51
Shivan Khullar <shivankhullar@gmail.com>
2023-05-31 18:51:36 -0400

    added threshold fraction for child as a param

--- linker/linker_structs.h
+++ linker/linker_structs.h
@@ -8,7 +8,7 @@ struct Params{
         std::string cloud_prefix, path, filename_base_prefix, filename_base_suffix, dat_filename_base_prefix, dat_filename_base_suffix;
         std::string write_filename_base_prefix, write_filename_base_suffix, linker_output_filename_prefix;
         std::string file_arch_root, file_arch_cloud_subgroup, file_arch_masses_field, file_arch_pIDs_field;
-        int particle_lower_limit;
+        int particle_lower_limit; double threshold_frac_for_child;
 };
 
 struct Group_struct{

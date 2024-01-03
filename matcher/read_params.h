01003be8dc6970b107ad33d4a5b4cdaf0473c44c
Shivan Khullar <shivankhullar@gmail.com>
2024-01-02 18:17:13 -0700

    missing #endif fix

--- matcher/read_params.h
+++ matcher/read_params.h
@@ -1,14 +1,17 @@
-//#ifndef READ_PARAMS_H
-//#define READ_PARAMS_H
+#ifndef READ_PARAMS_H
+#define READ_PARAMS_H
+
+#include "structs_and_classes.h"
+//#include "matcher_functions.h"
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
 
-#include "structs_and_classes.h"
-#include "matcher_functions.h"
 
 void trim(std::string& s);
 void printParams(const Params& params);
 bool parseParams(const std::string& filename, Params& params, const std::string& name);
+
+#endif
